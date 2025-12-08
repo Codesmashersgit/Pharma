a72adf2ac0b5d07ce1213cf2f9453564d4d68699
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-12-09 01:01:06 +0530

    Created Footer Section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import pic from "../assets/pics/17646339f71940f72e73b1a9d9e085a58808153f.png"
+
+const Footer = () => {
+  return (
+    <div>
+        <img src={pic}/>
+    </div>
+  )
+}
+
+export default Footer
