f0e767abd77ae92db9d97f15b395f5d93cbb8fcf
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-12-09 16:42:48 +0530

    Added data files

---
+++ src/data/data.js
@@ -0,0 +1,93 @@
+
+import img1 from "../assets/pics/3e62a1a0e3e9d6daebb29f401b2798bd26b4c58c.png";
+import img2 from "../assets/pics/cba8c727891af4c9bdabee359ec88593218676af.png";
+import img3 from "../assets/pics/dbc3cff3fb54ae33de869b36fcbe89ca07fe160a.png";
+
+export const products = [
+  {
+    id: 1,
+    image: img1,
+    rating: 5,
+    price: 649,
+    description: "Amrutam Kuntal care Hair Spa | Do- it yourself Hair Treatment"
+  },
+  {
+    id: 2,
+    image: img2,
+    rating: 5,
+    price: 549,
+    description: "Amrutam Kuntal Care Herbal Shampoo | Healthy, Natural and Dynamic Hair"
+  },
+  {
+    id: 3,
+    image: img3,
+    rating: 5,
+    price: 299,
+    description: "Amrutam Nari Sondarya Malt | Complete Care For Women's Health And Beauty"
+  },
+];
+
+import deco1 from "../assets/pics/Group (1).svg";
+import deco2 from "../assets/pics/Frame 1000007060.png";
+import deco3 from "../assets/pics/Frame 1000007067.png";
+import deco4 from "../assets/pics/Frame 1000007061.svg";
+import deco5 from "../assets/pics/Group.svg";
+
+// BUTTON IMAGES
+import playStore from "../assets/pics/image 68.svg";
+import appStore from "../assets/pics/image 67.svg";
+
+// BENEFIT ICON
+import benefitIcon from "../assets/pics/Frame 427319082.svg";
+
+export const downloadImages = {
+  deco1,
+  deco2,
+  deco3,
+  deco4,
+  deco5,
+  playStore,
+  appStore,
+  benefitIcon,
+};
+
+// BENEFITS LIST
+export const benefits = [
+  "Access to Prescriptions",
+  "Track health efficiently",
+  "Direct Chat with Doctors",
+  "In-app reminders for consultations",
+];
+
+import logo from "../assets/pics/Amrutam.png";
+import cartIcon from "../assets/pics/Frame 1171275545.png";
+import bellIcon from "../assets/pics/Frame 1000007496.png";
+import profileIcon from "../assets/pics/Untitled-1 [Recovered]-04 2.png";
+import menuArrow from "../assets/pics/Frame 1171275530.png";
+import storeBanner from "../assets/pics/Model.png";
+
+import catAll from "../assets/pics/All.svg";
+import catHair from "../assets/pics/Hair.svg";
+import catSkin from "../assets/pics/Skin care.svg";
+import catStomach from "../assets/pics/Stomach.svg";
+import catBones from "../assets/pics/Bones.svg";
+import catImmunity from "../assets/pics/Immunity.svg";
+import catHairCare from "../assets/pics/hair care.svg";
+
+export const navbarImages = {
+  logo,
+  cartIcon,
+  bellIcon,
+  profileIcon,
+  menuArrow,
+  storeBanner,
+  categories: [
+    catAll,
+    catHair,
+    catSkin,
+    catStomach,
+    catBones,
+    catImmunity,
+    catHairCare,
+  ],
+};
